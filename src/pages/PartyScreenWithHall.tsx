13e55b10614882c8b1d8e473fe083e5e35b7c7ff
Maxence Nho <78363268+PiKwaii93@users.noreply.github.com>
2026-08-30 17:50:04 +0200

    feat: show Photo Hunt wall on party screen

--- src/pages/PartyScreenWithHall.tsx
+++ src/pages/PartyScreenWithHall.tsx
@@ -1,12 +1,17 @@
 import { useParty } from '../features/party/PartyContext'
 import HallOfFameScreen from './HallOfFameScreen'
 import PartyScreen from './PartyScreen'
+import PhotoHuntScreen from './PhotoHuntScreen'
 
 function PartyScreenWithHall() {
   const { settings, loading } = useParty()
 
   if (!loading && settings.phase === 'ended') {
     return <HallOfFameScreen />
+  }
+
+  if (!loading && settings.featuredModule === 'photos') {
+    return <PhotoHuntScreen />
   }
 
   return <PartyScreen />
